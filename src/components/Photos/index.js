7717286f57354edbc28429fcfdfd6085df08e229
Mike Easley <michael.easley.c@iamplus.com>
2017-06-05 12:48:07 -0700

    Working now, but need to add close button

--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -27,39 +27,40 @@ export default class Photos extends Component {
   }
 
   onPhotoClick = (imageUrl) => {
-
 	  return () => this.setState({selectedImageUrl: imageUrl })
   }
 
   render() {
-	  let selectedImageUrl = this.state.selectedImageUrl;
       return (
         <div>
           <div onClick={(e)=>this.onPhotoHeaderClick(e)}>
             <div className="tmpHeader"> <div className="arrow">{'<'}</div> <div className="photos-header-cp">Photos</div></div>
           </div>
           <div className="photos">
-          {this.state.imageArray.map((image, index) => 	{
-            return(
-
-				<div key={index}>
-              <div  className="image-crop">
-                  <a onClick={this.onPhotoClick(image)}>
-				  	<img src={image} alt={image} width="358"/>
-					</a>
-              </div>
-			  {selectedImageUrl ? this.renderSelectedImage(selectedImageUrl) : ''}
-				</div>
-            );
-          })}
+		  {this.state.selectedImageUrl ? this.renderSelectedImage(this.state.selectedImageUrl)  : this.renderThumbnails(this.state.imageArray) }
           </div>
         </div>
       );
   }
 
+  renderThumbnails = (images) => {
+          return images.map((image, index) => {
+            return(
+				<div key={index}>
+					<div className="image-crop">
+						<a onClick={this.onPhotoClick(image)}>
+						  	<img src={image} alt={image} width="358"/>
+						</a>
+		              </div>
+				</div>
+            );
+		});
+
+  }
+
   renderSelectedImage = (imageUrl) => {
 	  return (
-		  <div>
+		  <div onClick={() => {this.setState({selectedImageUrl: ''})}}>
 			  <img src={imageUrl} alt={imageUrl} />
 		  </div>
 	  )
