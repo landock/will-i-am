90cfda24b05ee0996db5d189207691e7d12a99d6
Warren Curtis <warren.curtis@iamplus.com>
2017-06-07 11:45:41 -0700

    add facebook component

---
+++ src/components/Facebook/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import AppHeader from '../AppHeader';
+
+const iframeSrcUrl = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/20522460&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true';
+
+export default class Music extends Component {
+
+	onMusicHeaderClick = () => {
+	    this.props.closeApp();
+	}
+
+	render() {
+	    return (
+			<div className="Music">
+				<AppHeader name="music" onHeaderClick={() => this.onMusicHeaderClick()} />
+			        <iframe
+						width="100%"
+						height="620"
+						scrolling="no"
+						frameBorder="no"
+						title="music"
+				        src={iframeSrcUrl}
+			        />
+			</div>
+    );
+  }
+}
