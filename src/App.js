9f9cc737d335d4dc5fb7885bc1f087fb47eef5be
Mike Easley <michael.easley.c@iamplus.com>
2017-06-07 15:38:27 -0700

    got it basically working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Slider from 'react-slick'
-import Messages from './components/Messages'
-import Music from './components/Music'
-import Photos from './components/Photos'
+import Slider from 'react-slick';
+import Messages from './components/Messages';
+import Music from './components/Music';
+import Photos from './components/Photos';
 
 import messagesIcon from './images/messages-icon.png';
 import musicIcon from './images/music-icon.png';
@@ -19,72 +19,92 @@ import './App.css';
 class App extends Component {
 
   constructor(props) {
-	  super(props);
-	  this.state = {
-      isHomeDisplayed     : true,
+    super(props);
+    this.state = {
+      isHomeDisplayed: true,
       areMessagesDisplayed: false,
-      isMusicDisplayed    : false,
-      arePhotosDisplayed  : false
+      isMusicDisplayed: false,
+      arePhotosDisplayed: false,
     };
+
+    this.handleMessagesClick = this.handleMessagesClick.bind(this);
+    this.handleMusicClick = this.handleMusicClick.bind(this);
+    this.handlePhotosClick = this.handlePhotosClick.bind(this);
   }
 
-  handleMessagesClick = () => {
+  handleMessagesClick() {
     this.setState({
-      isHomeDisplayed     : !this.state.isHomeDisplayed,
-      areMessagesDisplayed: !this.state.areMessagesDisplayed
+      isHomeDisplayed: !this.state.isHomeDisplayed,
+      areMessagesDisplayed: !this.state.areMessagesDisplayed,
     });
   }
 
-  handleMusicClick = () => {
+  handleMusicClick() {
     this.setState({
-      isHomeDisplayed : !this.state.isHomeDisplayed,
-      isMusicDisplayed: !this.state.isMusicDisplayed
+      isHomeDisplayed: !this.state.isHomeDisplayed,
+      isMusicDisplayed: !this.state.isMusicDisplayed,
     });
   }
 
-  handlePhotosClick = () => {
+  handlePhotosClick() {
     this.setState({
-      isHomeDisplayed   : !this.state.isHomeDisplayed,
-      arePhotosDisplayed: !this.state.arePhotosDisplayed
+      isHomeDisplayed: !this.state.isHomeDisplayed,
+      arePhotosDisplayed: !this.state.arePhotosDisplayed,
     });
   }
 
   render() {
-	/*
-	we can probably wrap this shit into a base slider component with these
-	as the defaults at some point. then we can swap it out later if react slick sucks
-	cuz we have the fucking slider adapter
-	*/
-    let settings = {
-          autoplay:false,
-          draggable: true,
-          infinite: false,
-          arrows: false,
-          centerMode: false,
-          slidesToShow:1,
-          slidesToScroll:1,
-          touchMove: false,
+    const settings = {
+      autoplay: false,
+      draggable: true,
+      infinite: false,
+      arrows: false,
+      centerMode: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      touchMove: false,
     };
 
     const homeSlider = (
       <Slider {...settings} class="slider">
         <div className="screen">
           <div className="icon-wrapper">
-            <a href="http://www.twitter.com/iamwill" target="_blank" rel="noopener noreferrer" ><img className="icon" alt="icon" src={twitterIcon}  /></a>
-            <a href="https://www.facebook.com/william" target="_blank" rel="noopener noreferrer"><img className="icon" alt="icon" src={facebookIcon} /></a>
-            <a href="https://instagram.com/iamwill" target="_blank" rel="noopener noreferrer"><img className="icon" alt="icon" src={instagramIcon}/></a>
-            <a onClick={this.handleMusicClick}  id="music"   ><img className="icon" alt="icon" src={musicIcon}   /></a>
-            <a onClick={this.handlePhotosClick} id="photos"   ><img className="icon" alt="icon" src={photosIcon}   /></a>
-            <a href="https://iamplus.com" target="_blank" rel="noopener noreferrer"><img className="icon" alt="icon" src={iamIcon}/></a>
-            <a href="https://ill.i.am/" target="_blank" rel="noopener noreferrer"><img className="icon" alt="icon" src={illIcon}/></a>
-            <a href="http://iamangelfoundation.org/" target="_blank" rel="noopener noreferrer"><img className="icon" alt="icon" src={angelIcon}/></a>
+            <a href="http://www.twitter.com/iamwill" target="_blank" rel="noopener noreferrer" >
+              <img className="icon" alt="icon" src={twitterIcon} />
+            </a>
+            <a href="https://www.facebook.com/william" target="_blank" rel="noopener noreferrer">
+              <img className="icon" alt="icon" src={facebookIcon} />
+            </a>
+            <a href="https://instagram.com/iamwill" target="_blank" rel="noopener noreferrer">
+              <img className="icon" alt="icon" src={instagramIcon} />
+            </a>
+            <a role="button" tabIndex={0} onClick={this.handleMusicClick} id="music">
+              <img className="icon" alt="icon" src={musicIcon} /></a>
+            <a role="button" tabIndex={0} onClick={this.handlePhotosClick} id="photos" >
+              <img className="icon" alt="icon" src={photosIcon} />
+            </a>
+            <a href="https://iamplus.com" target="_blank" rel="noopener noreferrer">
+              <img className="icon" alt="icon" src={iamIcon} />
+            </a>
+            <a href="https://ill.i.am/" target="_blank" rel="noopener noreferrer">
+              <img className="icon" alt="icon" src={illIcon} />
+            </a>
+            <a href="http://iamangelfoundation.org/" target="_blank" rel="noopener noreferrer">
+              <img className="icon" alt="icon" src={angelIcon} />
+            </a>
           </div>
         </div>
         <div className="screen">
           <div className="icon-wrapper">
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" ><img className="icon" alt="icon" src={twitterIcon}  /></a>
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><img className="icon" alt="icon" src={facebookIcon} /></a>
-            <a onClick={this.handlePhotosClick}                                     ><img className="icon" alt="icon" src={instagramIcon}/></a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" >
+              <img className="icon" alt="icon" src={twitterIcon} />
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <img className="icon" alt="icon" src={facebookIcon} />
+            </a>
+            <a role="button" tabIndex={0} onClick={this.handlePhotosClick} >
+              <img className="icon" alt="icon" src={instagramIcon} />
+            </a>
           </div>
         </div>
       </Slider>
@@ -93,27 +113,28 @@ class App extends Component {
     const homeFooter = (
       <div className="menu-bottom">
         <div className="icon-wrapper">
-          <a onClick={this.handleMessagesClick} id="messages"><img className="icon" alt="icon" src={messagesIcon}/></a>
+          <a role="button" tabIndex={0} onClick={this.handleMessagesClick} id="messages">
+            <img className="icon" alt="icon" src={messagesIcon} />
+          </a>
         </div>
       </div>
     );
 
     return (
       <div className="App">
-        <div className="nav"></div>
-
+        <div className="nav" />
         <div className="wrapper">
           <div className="phone-wrapper">
             <div className="crop">
               {/* Home Screen */}
-              {this.state.isHomeDisplayed      ? homeSlider                                      : ''}
-              {this.state.areMessagesDisplayed ? <Messages closeApp={this.handleMessagesClick}/> : ''}
-              {this.state.isMusicDisplayed     ? <Music    closeApp={this.handleMusicClick}/>    : ''}
-              {this.state.arePhotosDisplayed   ? <Photos   closeApp={this.handlePhotosClick}/>   : ''}
+              {this.state.isHomeDisplayed ? homeSlider : ''}
+              {this.state.areMessagesDisplayed ? <Messages closeApp={this.handleMessagesClick} /> : ''}
+              {this.state.isMusicDisplayed ? <Music closeApp={this.handleMusicClick} /> : ''}
+              {this.state.arePhotosDisplayed ? <Photos closeApp={this.handlePhotosClick} /> : ''}
             </div>
             <div>
               {/* Footer */}
-              {this.state.isHomeDisplayed      ? homeFooter                                      : ''}
+              {this.state.isHomeDisplayed ? homeFooter : ''}
             </div>
           </div>
         </div>
--- src/api/flickr.js
+++ src/api/flickr.js
@@ -1,10 +1,27 @@
 const API_KEY = 'cf074b37903b6dcd889ac7d18549e361';
 const FLICKR_USER_ID = '155353158@N06';
-const API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=${API_KEY}&user_id=${FLICKR_USER_ID}&format=json&nojsoncallback=1`;
+const API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=${API_KEY}&user_id=${FLICKR_USER_ID}&extras=media&format=json&nojsoncallback=1`;
 
+export const fetchImages = () => fetch(API_ENDPOINT)
+  .then(response => response.json()
+  .then((json) => {
+    const mediaUrls = [];
 
-export default function fetchImages() {
-  return fetch(API_ENDPOINT).then(response => response.json().then(json => json.photos.photo.map(
-        ({ farm, server, id, secret }) => `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`,
-      )));
-}
+    json.photos.photo.forEach(({ farm, server, id, secret, media }) => {
+      if (media === 'photo') {
+        mediaUrls.push({
+          type: 'photo',
+          url: `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`,
+        });
+      } else {
+        mediaUrls.push({
+          type: 'video',
+          url: `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`,
+          videoUrl: `https://www.flickr.com/photos/${FLICKR_USER_ID}/${id}/play/site/${secret}/`,
+        });
+      }
+    });
+    console.log(mediaUrls);
+    return mediaUrls;
+  },
+));
