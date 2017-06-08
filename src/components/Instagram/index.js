5f6b591692a6f591edb98d0e1a2963e35abc12cf
Mike Easley <michael.easley.c@iamplus.com>
2017-06-08 16:22:36 -0700

    everything is merged now

--- src/components/Instagram/index.js
+++ src/components/Instagram/index.js
@@ -1,36 +1,17 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
-import { fetchInstagram } from '../../api/instagram';
+
 import AppHeader from '../AppHeader';
 
 export default class Instagram extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      selectedItem: '',
-      userProfile: {}
-    };
+    this.state = { selectedItem: '' };
 
     this.onPhotoHeaderClick = this.onPhotoHeaderClick.bind(this);
     this.onThumbnailClick = this.onThumbnailClick.bind(this);
   }
-
-
-  componentDidMount() {
-    fetchInstagram()
-    .then((response) => {
-      // limit array to 18 items so they all fit in the screen
-      response.items.splice(-2, 2);
-
-      this.setState({
-        dataArray: response.items,
-        userProfile: response.items[0].user
-      });
-    })
-    .catch(err => console.log(`Fetch Images Error: ${err}`));
-  }
-
 
   onPhotoHeaderClick() {
     this.props.closeApp();
@@ -42,19 +23,23 @@ export default class Instagram extends Component {
 
   renderThumbnails(dataArray) {
     return dataArray.map((data, index) => (
-      <div key={index}className="image-crop">
-        <a href="javascript:void(0)" onClick={this.onThumbnailClick(data)}>
-          <img src={data.images.standard_resolution.url} role="presentation" alt={data.images.standard_resolution.url} />
+      <div key={index} className="image-crop">
+        <a role="button" tabIndex={0} onClick={this.onThumbnailClick(data)}>
+          <img
+            src={data.images.standard_resolution.url}
+            role="presentation"
+            alt={data.images.standard_resolution.url}
+          />
         </a>
       </div>
     ));
   }
 
   renderSelectedItem(data) {
-    if (data.type === "video") {
-      return(
+    if (data.type === 'video') {
+      return (
         <div role="button" tabIndex={0} className="full-width" onClick={() => this.setState({ selectedItem: '' })}>
-          <ReactPlayer url={data.videos.standard_resolution.url} playing width="auto" height="100%"/>
+          <ReactPlayer url={data.videos.standard_resolution.url} playing width="auto" height="100%" />
           <div>
             <p className="user-count"><strong>{data.video_views.toLocaleString()}</strong> views</p>
             <p className="sub"><strong>{data.user.username}</strong> {data.caption.text}</p>
@@ -75,13 +60,15 @@ export default class Instagram extends Component {
   }
 
   render() {
+    const { userProfile } = this.props;
+
     return (
       <div className="Photos">
         <AppHeader name="instagram" onHeaderClick={() => this.onPhotoHeaderClick()} />
         <div className="instagramHeader">
           <div className="profile-info">
-          <img src={this.state.userProfile.profile_picture} alt={this.state.userProfile.profile_picture} />
-            <p><strong>{this.state.userProfile.full_name}</strong></p>
+            <img src={userProfile.profile_picture} alt={userProfile.profile_picture} />
+            <p><strong>{userProfile.full_name}</strong></p>
           </div>
         </div>
         <div className="instagram-wrapper">
