5e8de4f563122e52b9d26e054fc3135df8383d1c
Mike Easley <michael.easley.c@iamplus.com>
2017-06-12 15:57:35 -0700

    Added prop so that AppHeader can accept imageSrc to use as a header, specifically so Instagram app has the logo instead of text.

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-export default function AppHeader({ name, onHeaderClick }) {
+export default function AppHeader({ name, onHeaderClick, imgHeaderSrc }) {
   return (
     <div role="button" tabIndex={0} className="AppHeader" onClick={onHeaderClick}>
       <div className="tmpHeader">
         <div className="arrow">{'<'}</div>
-        <div className="header-cp">{name}</div>
+        <div className="header-cp">
+          {
+            imgHeaderSrc && !name
+              ? (<img className="app-header-img" alt={imgHeaderSrc} src={imgHeaderSrc} />)
+              : name
+          }
+        </div>
       </div>
     </div>
   );
