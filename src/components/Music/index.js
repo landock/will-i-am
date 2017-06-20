6c3b4777b30ede25fa34764fee769ddd3227aa01
Mike Easley <michael.easley.c@iamplus.com>
2017-06-19 17:54:11 -0700

    basics of the "track player" is working.

--- src/components/Music/index.js
+++ src/components/Music/index.js
@@ -1,26 +1,69 @@
 import React, { Component } from 'react';
-import AppHeader from '../AppHeader';
+import ReactPlayer from 'react-player';
 
-const iframeSrcUrl = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/20522460&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true';
+import AppHeader from '../AppHeader';
+import playIcon from '../../images/play.svg';
+import fastForwardIcon from '../../images/forward.svg';
 
 export default class Music extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { audioUrl: '' };
+		this.onTrackClick = this.onTrackClick.bind(this);
+	}
 
   onMusicHeaderClick() {
     this.props.closeApp();
   }
 
-  render() {
+	onTrackClick(track) {
+		console.log(track);
+
+		this.setState({
+			audioUrl: track.streamUrl,
+			currentTrackArtwork: track.artworkUrl,
+			currentTrackTitle: track.title
+		})
+	}
+
+	render() {
+		const { closeApp, tracks } = this.props;
+		const player = tracks.map(track => (
+			<button className="play-track-btn" key={track.id} onClick={() => this.onTrackClick(track)}>
+				<img src={track.artworkUrl}/>
+				<span className="track-title">{track.title}</span>
+				<br/>
+				<span className="track-artist">{track.artist}</span>
+				{
+					this.state.audioUrl
+						? <ReactPlayer onEnded={() => this.setState({ audioUrl: '' })} url={this.state.audioUrl} height="0px"
+						               playing/>
+						: ''
+				}
+			</button>
+
+		));
     return (
       <div className="Music">
-	      <AppHeader title="music" onHeaderClick={() => this.onMusicHeaderClick()}/>
-        <iframe
-          width="100%"
-          height="620"
-          scrolling="no"
-          frameBorder="no"
-          title="music"
-          src={iframeSrcUrl}
-        />
+	      <AppHeader title="music" onHeaderClick={() => closeApp()}/>
+	      <div className="track-wrapper">
+		      {player}
+	      </div>
+	      {
+		      this.state.currentTrackArtwork
+			      ? (
+				      <div className="current-track-player">
+					      <img src={this.state.currentTrackArtwork}/>
+					      <span className="current-track-title">{this.state.currentTrackTitle}</span>
+					      <div className="controls">
+						      <img src={playIcon} className="play-icon"/>
+						      <img src={fastForwardIcon} className="fast-forward-icon"/>
+					      </div>
+				      </div>
+			      )
+			      : ''
+	      }
       </div>
     );
   }
