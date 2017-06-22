e686740b0180c71cb21c9be90e8b5be833844959
Mike Easley <michael.easley.c@iamplus.com>
2017-06-22 01:04:10 -0700

    Fixed music app in mobile safari; Added new album art; Added new method for getting playlists from soundcloud;

--- src/api/soundcloud.js
+++ src/api/soundcloud.js
@@ -1,11 +1,28 @@
 const clientId = 'xbNbV9X2x73kHAqADNhr5oJa3n72CBPt';
-const userId = '4788130';//'839528';
+const userId = '839528';//'4788130';
 const baseUrl = 'https://api.soundcloud.com';
 const playlistId = '331992373';
 const usersApi = `${baseUrl}/users/${userId}/tracks?client_id=${clientId}`;
 const playlistsApi = `${baseUrl}/playlists/${playlistId}/tracks?client_id=${clientId}`;
 
-export function fetchTracks() {
+export function fetchTracksFromUser() {
+	return fetch(usersApi)
+		.then(response => response.json())
+		.then(json => json.map(track => {
+			return {
+				id: track.id,
+				title: track.title,
+				artist: track.user.username,
+				artistId: track.user.id,
+				artworkUrl: track.artwork_url,
+				streamUrl: track.streamable ? `${track.stream_url}?client_id=${clientId}` : '',
+				downloadUrl: track.downloadable ? `${track.download_url}?client_id=${clientId}` : '',
+			}
+		}))
+		.catch(err => console.log('Error fetching user tracks from Soundclound: ', err));
+}
+
+export function fetchTracksFromPlaylist() {
 	return fetch(playlistsApi)
 		.then(response => response.json())
 		.then(json => json.map(track => {
@@ -19,9 +36,5 @@ export function fetchTracks() {
 				downloadUrl: track.downloadable ? `${track.download_url}?client_id=${clientId}` : '',
 			}
 		}))
-		.catch(err => console.log('Error fetching data from Soundclound: ', err));
-}
-
-export function fetchPlaylistTracks() {
-
+		.catch(err => console.log('Error fetching playlist tracks from Soundclound: ', err));
 }
--- src/components/Music/index.js
+++ src/components/Music/index.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
-import ReactPlayer from 'react-player';
 
 import AppHeader from '../AppHeader';
 
 import playIcon from '../../images/pause.svg';
 import pauseIcon from '../../images/play.svg';
 import fastForwardIcon from '../../images/forward.svg';
-import audio from '../../images/audio.svg';
+import audioIcon from '../../images/audio.svg';
 import missingArt from '../../images/missing-album-art-icon.png';
 
 export default class Music extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { audioUrl: '', isPlaying: false };
+		this.state = {
+			audioUrl: '',
+			isPlaying: false
+		};
+
+		this.audioEl = null;
+
 		this.onTrackClick = this.onTrackClick.bind(this);
 		this.handlePlayClick = this.handlePlayClick.bind(this);
 		this.onMusicHeaderClick = this.onMusicHeaderClick.bind(this);
+	}
+
+	componentDidUpdate() {
+		if (!this.audioEl) return;
+
+		let isReallyPlaying = this.audioEl.currentTime > 0 && !this.audioEl.paused && !this.audioEl.ended && this.audioEl.readyState > 2;
+
+		if (this.state.isPlaying && !isReallyPlaying) {
+			try {
+				this.audioEl.play();
+			}
+			catch (e) {
+				console.log(e)
+			}
+		} else {
+			try {
+				this.audioEl.pause();
+			}
+			catch (e) {
+				console.log(e)
+			}
+		}
 	}
 
   onMusicHeaderClick() {
@@ -29,18 +56,19 @@ export default class Music extends Component {
 
 	onTrackClick(track) {
 		this.setState({
-			audioUrl: track.streamUrl,
+				audioUrl: track.streamUrl || track.downloadUrl,
 			isPlaying: true,
 			currentTrackArtwork: track.artworkUrl || missingArt,
-			currentTrackTitle: track.title
-		})
+				currentTrackTitle: track.title,
+			}
+		)
 	}
 
 	render() {
 		const { closeApp, tracks } = this.props;
 		const player = tracks.map(track => {
 			const linearGradient = this.state.currentTrackTitle === track.title && this.state.isPlaying
-				? 'linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8))'
+				? 'linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.5))'
 				: 'linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0))';
 			const artworkStyle = {
 				background: `${linearGradient}, url('${track.artworkUrl}')`,
@@ -51,7 +79,9 @@ export default class Music extends Component {
 					<div style={artworkStyle} className="track-art">
 						{
 							this.state.currentTrackTitle === track.title && this.state.isPlaying
-								? <img src={audio} style={{ width: 'auto', height: '20px', display: 'block', margin: '33% auto 0' }}/>
+								//TODO: make most of this styling a class
+								? <img src={audioIcon} alt={audioIcon}
+								       style={{ width: 'auto', height: '20px', display: 'block', margin: '33% auto 0' }}/>
 								: ''
 						}
 					</div>
@@ -63,30 +93,34 @@ export default class Music extends Component {
 			)
 		});
 
-		const currentlyPlayingOpenClass = this.state.isPlaying ? 'track-wrapper player-open' : 'track-wrapper';
+		const currentlyPlayingOpenClass = this.state.isPlaying
+			? 'track-wrapper player-open'
+			: 'track-wrapper';
+
     return (
       <div className="Music">
 	      <AppHeader title="music" onHeaderClick={() => closeApp()}/>
 	      <div className={currentlyPlayingOpenClass}>
 		      {player}
-		      {
-			      this.state.audioUrl
-				      ? <ReactPlayer
-					      onEnded={() => this.setState({ audioUrl: '' })}
-					      url={this.state.audioUrl}
-					      height="0"
-					      width="0"
-					      playing={this.state.isPlaying}
-				      />
-				      : ''
-		      }
+		      <audio
+			      ref=
+				      {
+					      (audio) => {
+						      if (!audio) return;
+
+						      this.audioEl = audio;
+						      return this.audioEl.click();
+					      }
+				      }
+			      src={this.state.audioUrl}
+		      />
 	      </div>
 	      {
 		      this.state.currentTrackArtwork
 			      ? (
 				      <div className="current-track-player">
 					      <img alt={this.state.currentTrackArtwork} src={this.state.currentTrackArtwork}/>
-					      <p className="current-track-title">{this.state.currentTrackTitle}</p>
+					      <div className="current-track-title"><span>{this.state.currentTrackTitle}</span></div>
 					      <div className="controls">
 						      <button onClick={this.handlePlayClick}>
 							      <img alt={playIcon} src={!this.state.isPlaying ? pauseIcon : playIcon} className="play-icon"/>
