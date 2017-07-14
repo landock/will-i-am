95e57e3e51c6022c9b85596839dc433e3f13c419
Warren Curtis <warren.curtis@iamplus.com>
2017-07-14 16:25:55 -0700

    update api keys flickr gmail soundcloud

--- src/api/calendar.js
+++ src/api/calendar.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
-const GOOGLE_API_KEY = 'AIzaSyAfy7djlPQFWfX4TqKttxtaZQD8AQBojH4';
-const CALENDAR_ID = 'k27qtflciqu8m61qsiulro1mr0@group.calendar.google.com';
+const GOOGLE_API_KEY = 'AIzaSyAthXrwr4IqBusBkzTv4Se4y7uvtkPNmgU';
+const CALENDAR_ID = 'ck4h6bgu515hrq4kja38rnluic@group.calendar.google.com';
 const getApiUrl = (calendarId, apiKey) => `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/?key=${apiKey}&singleEvents=true&orderBy=startTime`;
 
 export function fetchEvents() {
--- src/api/flickr.js
+++ src/api/flickr.js
@@ -1,5 +1,5 @@
 const API_KEY = 'cf074b37903b6dcd889ac7d18549e361';
-const FLICKR_USER_ID = '155353158@N06';
+const FLICKR_USER_ID = '82970222@N03';
 const API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=${API_KEY}&user_id=${FLICKR_USER_ID}&extras=media&format=json&nojsoncallback=1`;
 
 export const fetchImages = () => fetch(API_ENDPOINT)
